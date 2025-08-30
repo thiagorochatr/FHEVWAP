7008d2950052ecff8d91dba44b0ccb42aae12217
thiagorochatr <thiagord2016@gmail.com>
2025-08-29 22:03:36 -0300

    readme done

--- packages/site/abi/FHECounterAddresses.ts
+++ packages/site/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0x0000000000000000000000000000000000000000", chainId: 11155111, chainName: "sepolia" },
-  "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
+  "11155111": { address: "0xe5f1B70DBa3AAaEC57f651dB06582C6c30Eb0009", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x5A88083408e7eEF95a4C734578c70726eC2C8C63", chainId: 31337, chainName: "hardhat" },
 };
